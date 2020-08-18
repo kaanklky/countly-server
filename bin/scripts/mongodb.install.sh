47702533775eaee87bff39d447dc331f850d210c
Kaan Kölköy <kaanklky@gmail.com>
2020-08-18 23:03:28 +0300

    Add MongoDB repo for CentOS/RHEL 8

--- bin/scripts/mongodb.install.sh
+++ bin/scripts/mongodb.install.sh
@@ -370,6 +370,13 @@ baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.6/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc" > /etc/yum.repos.d/mongodb-org-3.6.repo
+        elif grep -q -i "release 8" /etc/redhat-release ; then
+            echo "[mongodb-org-3.6]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/8/mongodb-org/3.6/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc" > /etc/yum.repos.d/mongodb-org-3.6.repo
         fi
         yum install -y mongodb-org
     fi
